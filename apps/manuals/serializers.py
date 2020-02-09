487e31aa5b01b42baddff57739527716bfd9bbe9
AntonCherepov <anton.cherepov.97@gmail.com>
2020-02-09 10:55:04 +0300

    Refactor manuals serializers

--- apps/manuals/serializers.py
+++ apps/manuals/serializers.py
@@ -1,25 +1,17 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from manuals.models import City
 
 
-class CitySerializer(serializers.ModelSerializer):
+class CitySerializer(ModelSerializer):
 
     class Meta:
         model = City
-        fields = (
-            "id",
-            "name",
-            "description",
-        )
+        fields = "__all__"
 
 
-class MasterTypeSerializer(serializers.ModelSerializer):
+class MasterTypeSerializer(ModelSerializer):
 
     class Meta:
         model = City
-        fields = (
-            "id",
-            "name",
-            "description",
-        )
+        fields = "__all__"
