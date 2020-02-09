5fed6beb7966cf50d42741cebf415814328541f9
kanumba <canummba@mail.ru>
2020-02-09 15:25:41 +0300

    first test

--- apps/personal_account/tests.py
+++ apps/personal_account/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTests(TestCase):
+
+    def test_users(self):
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
