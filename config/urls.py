50d6c89154cc0790cebee6440ff84aeca7417357
AntonCherepov <anton.cherepov.97@gmail.com>
2020-02-09 11:19:49 +0300

    Change urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('manuals/', include('manuals.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
