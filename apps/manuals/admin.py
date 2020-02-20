145e05970d7073fc40c6d3c970ee142ced9e95e9
AntonCherepov <anton.cherepov.97@gmail.com>
2020-02-20 16:59:17 +0300

    Add admin panel to manuals and order apps

--- apps/manuals/admin.py
+++ apps/manuals/admin.py
@@ -1,3 +1,42 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
 
-# Register your models here.
+from manuals.models import (MasterType, UserType, OrderStatus,
+                            MasterStatus, UserStatus, City)
+
+
+class MasterTypeAdmin(ModelAdmin):
+
+    list_display = ("id", "name")
+
+
+class UserTypeAdmin(ModelAdmin):
+
+    list_display = ("code", "name")
+
+
+class UserStatusAdmin(ModelAdmin):
+
+    list_display = ("code", "name")
+
+
+class OrderStatusAdmin(ModelAdmin):
+
+    list_display = ("code", "name")
+
+
+class MasterStatusAdmin(ModelAdmin):
+
+    list_display = ("code", "name")
+
+
+class CityAdmin(ModelAdmin):
+
+    list_display = ("id", "name", "description")
+
+
+site.register(MasterType, MasterTypeAdmin)
+site.register(MasterStatus, MasterStatusAdmin)
+site.register(OrderStatus, OrderStatusAdmin)
+site.register(UserStatus, UserStatusAdmin)
+site.register(UserType, UserTypeAdmin)
+site.register(City, CityAdmin)
--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
 
-# Register your models here.
+from order.models import Order
+
+
+class OrderAdmin(ModelAdmin):
+
+    list_display = ("master_type", "status_code", "date_created",
+                    "date_modified", "request_date_from", "request_date_to",
+                    "city", "selection_date", "description",
+                    "client",)
+    filter_horizontal = ("photo",)
+
+
+site.register(Order, OrderAdmin)
