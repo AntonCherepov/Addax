6a2c8bce70e249351edd6ecd28824489a2708494
AntonCherepov <anton.cherepov.97@gmail.com>
2020-03-01 01:14:49 +0300

    fix: Change field values in OrderSerializer

--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -2,10 +2,13 @@ from rest_framework.serializers import ModelSerializer
 
 from order.models import Order
 from photos.serializers import PhotoSerializer
+from manuals.serializers import CitySerializer, MasterTypeSerializer
 
 
 class OrderSerializer(ModelSerializer):
     images = PhotoSerializer(many=True, read_only=True, source='photo')
+    city = CitySerializer(read_only=True)
+    master_type = MasterTypeSerializer(read_only=True)
 
     class Meta:
         model = Order
