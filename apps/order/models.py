b3af2bdbbfcf9b8624f2f35d7feb7193bf9713d1
AntonCherepov <anton.cherepov.97@gmail.com>
2020-03-08 17:51:07 +0300

    feat: Add function to get order by id

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,9 +1,21 @@
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import (Model, CharField, DateTimeField, IntegerField,
                               ForeignKey, ManyToManyField, CASCADE, SET_NULL,)
 
 from manuals.models import City, OrderStatus, ReplyStatus
 from personal_account.models import MasterType, ClientAccount, MasterAccount
 from photos.models import Photo
+
+
+def order_by_id(order_id=None):
+    if isinstance(order_id, int):
+        try:
+            order = Order.objects.get(id=order_id)
+            return order
+        except ObjectDoesNotExist:
+            raise ValidationError(f"No order by {order_id = }")
+    else:
+        raise ValidationError(f"Invalid order_id")
 
 
 class Order(Model):
