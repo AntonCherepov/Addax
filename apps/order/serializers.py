2c2156174ed5067c7ec256a3cde9b075d810a8b5
AntonCherepov <anton.cherepov.97@gmail.com>
2020-03-14 21:28:26 +0300

    fix: #8 Change fields in OrderSerializer

--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -3,17 +3,29 @@ from rest_framework.serializers import ModelSerializer
 
 from order.models import Order, Reply
 from photos.serializers import PhotoSerializer
-from manuals.serializers import CitySerializer, MasterTypeSerializer
 
 
 class OrderSerializer(ModelSerializer):
     images = PhotoSerializer(many=True, read_only=True, source='photo')
-    city = CitySerializer(read_only=True)
-    master_type = MasterTypeSerializer(read_only=True)
+    city = SerializerMethodField('get_city_name')
+    master_type = SerializerMethodField('get_master_type_name')
+    status = SerializerMethodField('get_status_name')
+
+    @staticmethod
+    def get_city_name(obj):
+        return obj.city.name
+
+    @staticmethod
+    def get_master_type_name(obj):
+        return obj.master_type.name
+
+    @staticmethod
+    def get_status_name(obj):
+        return obj.status_code.name
 
     class Meta:
         model = Order
-        exclude = ("photo",)
+        exclude = ("photo", "status_code", "client")
 
 
 class ReplySerializer(ModelSerializer):
