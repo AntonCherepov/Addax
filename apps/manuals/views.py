f63f66d8885e79a4236bc2b166314e5d8b338961
AntonCherepov <anton.cherepov.97@gmail.com>
2020-04-03 07:46:39 +0300

    refactor: Remove the initial data create function

--- apps/manuals/views.py
+++ apps/manuals/views.py
@@ -2,41 +2,8 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 
-from manuals.models import (City, OrderStatus, ReplyStatus,
-                            UserStatus, MasterStatus, MasterType, UserType)
+from manuals.models import (City, MasterType)
 from manuals.serializers import CitySerializer, MasterTypeSerializer
-
-
-def creator():
-    City(name="Воронеж").save()
-    OrderStatus.objects.bulk_create(
-        [OrderStatus(code="sm", name="Подбор мастеров"),
-         OrderStatus(code="ms", name="Выбран мастер"),
-         OrderStatus(code="сс", name="Отменена клиентом"),
-         OrderStatus(code="cm", name="Отменена мастером"),
-         OrderStatus(code="na", name="Клиент не приехал"),
-         OrderStatus(code="cs", name="Завершена успешно")])
-    UserType.objects.bulk_create([UserType(code="c", name="client"),
-                                  UserType(code="m", name="master")])
-    UserStatus.objects.bulk_create([UserStatus(code="rg", name="registered"),
-                                    UserStatus(code="cf", name="confirmed"),
-                                    UserStatus(code="bn", name="banned")])
-    MasterType.objects.bulk_create(
-        [MasterType(name="Визажист"),
-         MasterType(name="Косметолог"),
-         MasterType(name="Массажист"),
-         MasterType(name="Мастер по маникюру"),
-         MasterType(name="Мастер по наращиванию ресниц"),
-         MasterType(name="Парикмахер"),
-         MasterType(name="Мастер эпиляции")])
-    MasterStatus.objects.bulk_create([MasterStatus(code="vr", name="verified"),
-                                      MasterStatus(code="uv", name="unverified"),
-                                      MasterStatus(code="bn", name="banned")])
-    ReplyStatus.objects.bulk_create(
-        [ReplyStatus(code="sl", name="Выбран"),
-         ReplyStatus(code="cs", name="Рассматривается"),
-         ReplyStatus(code="rj", name="Отклонен"),
-         ReplyStatus(code="rc", name="Отозван")])
 
 
 class CityView(APIView):
@@ -54,7 +21,6 @@ class MasterTypeView(APIView):
 
     @staticmethod
     def get(request):
-        # creator()
         masters_types = MasterType.objects.all()
         serialized_masters = MasterTypeSerializer(masters_types, many=True)
         return Response({"masters_types": serialized_masters.data},
