0d5a696a01f779997a2892922d1f1d9ac34998e2
AntonCherepov <anton.cherepov.97@gmail.com>
2020-05-03 16:59:53 +0300

    fix: Add missing users.constants file

---
+++ apps/users/constants.py
@@ -0,0 +1,27 @@
+# UserType
+CLIENT = 'c'
+MASTER = 'm'
+USER_TYPE_CHOICES = [
+    (CLIENT, 'Client'),
+    (MASTER, 'Master'),
+]
+
+# UserStatus
+USER_REGISTERED = 'rg'
+USER_CONFIRMED = 'cf'
+USER_BANNED = 'bn'
+USER_STATUS_CHOICES = [
+    (USER_REGISTERED, 'registered'),
+    (USER_CONFIRMED, 'confirmed'),
+    (USER_BANNED, 'banned'),
+]
+
+# MasterStatus
+MASTER_VERIFIED = "vr"
+MASTER_UNVERIFIED = "uv"
+MASTER_BANNED = "bn"
+MASTER_STATUS_CHOICES = [
+    (MASTER_VERIFIED, 'verified'),
+    (MASTER_UNVERIFIED, 'unverified'),
+    (MASTER_BANNED, 'banned'),
+]
