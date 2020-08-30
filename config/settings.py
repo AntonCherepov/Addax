b196545c6043159521a1e95ed726a826c94533dc
AntonCherepov <anton.cherepov.97@gmail.com>
2020-08-30 15:32:06 +0300

    feat: Add celery 'send_email' and 'send_phone_mail' tasks

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,10 @@ APPEND_SLASH = False
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
---
+++ core/tasks.py
@@ -0,0 +1,17 @@
+from config.celery import app
+from core.smsc_api import SMSC
+
+from django.core.mail import send_mail
+
+from config.settings import EMAIL_HOST_USER
+
+
+@app.task(ignore_result=True)
+def send_email(recipient_list: list, message: str, title: str):
+    send_mail(title, message, EMAIL_HOST_USER, recipient_list)
+
+
+@app.task(ignore_result=True)
+def send_phone_mail(recipient_number: str, message: str):
+    sms = SMSC()
+    sms.send_sms("7"+recipient_number, message, sender="sms")
